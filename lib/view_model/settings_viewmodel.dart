ab582ce10fcde1638711e9381799e2f4ea887c2f
Vojta Netrh <vojtanetrh@gmail.com>
2025-03-24 19:59:30 +0100

    packages updated

--- lib/view_model/settings_viewmodel.dart
+++ lib/view_model/settings_viewmodel.dart
@@ -5,7 +5,6 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:personal_finance/data/database.dart';
 import 'package:personal_finance/model/exchange_rate.dart';
 import 'package:personal_finance/model/transaction.dart';
